65740d815d206b9cbcfa67d0760afd47288a7012
Terrac Skiens <tskiens@apple.com>
2017-12-06 23:23:59 -0800

    removing tmp from mounts

--- start_autorippr_docker.sh
+++ start_autorippr_docker.sh
@@ -19,9 +19,9 @@ sudo docker run -d \
     --device /dev/sr0 \
     -e PUID=1001 -e PGID=100 \
     -v ${LOCALDIR}:/config \
-    -v ${RIP}:/tmp/rip \
-    -v ${TV}:/tmp/tv \
-    -v ${MOVIES}:/tmp/movies \
+    -v ${RIP}:/rip \
+    -v ${TV}:/tv \
+    -v ${MOVIES}:/movies \
   ${IMAGE}:${VERSION}
 
 docker logs ${SERVICE}
