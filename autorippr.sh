71feaa3c3a62906b1ec558856517e697459ff282
Terrac Skiens <terracnosaur@gmail.com>
2018-01-02 22:16:02 -0800

    latest stuff

--- autorippr.sh
+++ autorippr.sh
@@ -16,7 +16,8 @@ sudo docker run -d \
     --restart=always \
     --name=${SERVICE} \
     --hostname=${HOSTNAME} \
-    --device=/dev/sr0:/dev/sr0:r \
+    --device=/dev/sr0:/dev/sr0:rw \
+    --cap-add=SYS_RAWIO \
     -e PUID=1001 -e PGID=1001 \
     -v ${LOCALDIR}:/config \
     -v ${RIP}:/tmp/rip \
