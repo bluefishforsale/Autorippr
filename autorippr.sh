e7c8011e1e9cab452700ba06ae2b9a1f265f85fe
Terrac Skiens <tskiens@apple.com>
2018-01-05 21:28:35 -0800

    adding a fancy drive choose

--- autorippr.sh
+++ autorippr.sh
@@ -8,7 +8,7 @@ RIP="/M2scratch/autorippr"
 TV="/data01/complete/tv"
 MOVIES="/data01/complete/movies"
 
-DRIVE=$(ls /dev/sr?)
+DRIVE=$(ls -1 /dev/sr* | head)
 
 docker stop ${SERVICE}
 docker rm ${SERVICE}
@@ -18,7 +18,7 @@ sudo docker run -d \
     --restart=always \
     --name=${SERVICE} \
     --hostname=${HOSTNAME} \
-    --device=${DRIVE}:/dev/sr0:rw \
+    --device=${DRIVE:-/dev/sr0}:/dev/sr0:rw \
     --cap-add=SYS_RAWIO \
     -e PUID=1001 -e PGID=1001 \
     -v ${LOCALDIR}:/config \
